756657d7af21f31c2651c25f8238ef6d926e14a2
Denil Gohil <167923026+Gohil01@users.noreply.github.com>
2025-11-27 15:15:54 +0000

    Add data loading and cleaning steps up to Step 3

--- first 5 row.R
+++ first 5 row.R
@@ -8,4 +8,28 @@ View(hapiness_report_2022)
 colnames(hapiness_report_2022)[2] <- "happiness score"
 print(head(hapiness_report_2022[, c("country", "happiness score", "income")]))
 
- n_countries <- length(unique(hapiness_report_2022$country)) #the number of unique countries
+
+# working directory and load the happiness dataset
+setwd("C:/Users/Owner/OneDrive - University of Hertfordshire/MCS/Team Research and Development/Grop project/Group-A23")
+
+hapiness_report_2022 <- read.csv("hapiness_report_2022.csv")
+
+# Quick check of the data
+head(hapiness_report_2022)
+colnames(hapiness_report_2022)
+
+# Clean column name and count unique countries
+# Rename 2nd column to 'happiness_score'
+colnames(hapiness_report_2022)[2] <- "happiness_score"
+
+# Number of unique countries in the dataset
+n_countries <- length(unique(hapiness_report_2022$country))
+n_countries
+
+# clean dataset (remove rows with missing income)
+hapiness_clean <- subset(hapiness_report_2022, !is.na(income))
+
+# Check how many rows and a summary
+nrow(hapiness_clean)
+summary(hapiness_clean)
+
