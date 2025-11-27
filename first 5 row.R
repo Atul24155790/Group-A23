dea19df851247fc2462bd52afe63c950007221bd
Denil Gohil <167923026+Gohil01@users.noreply.github.com>
2025-11-27 14:42:57 +0000

    Add n_countries calculation to R script

--- first 5 row.R
+++ first 5 row.R
@@ -1,4 +1,11 @@
-hapiness_report_2022 <- read.csv("D:/UK/UNI/team research and dev project/main assignment/hapiness_report_2022.csv")
+# Set working directory to the project folder (optional but handy)
+setwd("C:/Users/Owner/OneDrive - University of Hertfordshire/MCS/Team Research and Development/Grop project/Group-A23")
+
+# Read the CSV file in that folder
+hapiness_report_2022 <- read.csv("hapiness_report_2022.csv")  # check exact spelling of the file
+
 View(hapiness_report_2022)
 colnames(hapiness_report_2022)[2] <- "happiness score"
 print(head(hapiness_report_2022[, c("country", "happiness score", "income")]))
+
+ n_countries <- length(unique(hapiness_report_2022$country)) #the number of unique countries
